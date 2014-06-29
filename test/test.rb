805359b3103e667ab82710386818fa7a0dac3601
Brandon Mathis <brandon@imathis.com>
2014-06-29 18:33:47 -0500

    Added another relative path test

--- test/test.rb
+++ test/test.rb
@@ -4,8 +4,10 @@ has_failed = false
 system('rm -rf _site')
 system('jekyll build')
 
-diff = `diff _site/index.html expected.html`
+diff1 = `diff _site/index.html expected/index.html`
+diff2 = `diff _site/license.html expected/license.html`
 
-abort "Failed with diff: #{diff}" if diff.size > 0
+abort "Failed with diff: #{diff1}" if diff1.size > 0
+abort "Failed with diff: #{diff2}" if diff2.size > 0
 
 puts "passed".green
