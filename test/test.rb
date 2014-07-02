1f5d233e4c5593ae7f5cdc00ba9deaa44b5e6c67
Brandon Mathis <brandon@imathis.com>
2014-07-02 00:23:40 -0500

    Improved tests with a test suite

--- test/test.rb
+++ test/test.rb
@@ -1,13 +1,2 @@
-require 'colorator'
-has_failed = false
-
-system('rm -rf _site')
-system('jekyll build')
-
-diff1 = `diff _site/index.html expected/index.html`
-diff2 = `diff _site/license.html expected/license.html`
-
-abort "Failed with diff: #{diff1}" if diff1.size > 0
-abort "Failed with diff: #{diff2}" if diff2.size > 0
-
-puts "passed".green
+require './test_suite.rb'
+run_tests
